753d175977b158e558a0f34243e765bd65e37cdc
PACIFIC108 <138829228+PACIFIC108@users.noreply.github.com>
2025-04-07 20:56:06 +0530

    Update usermodel.js

--- models/usermodel.js
+++ models/usermodel.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/my_database')
+mongoose.connect('mongodb+srv://pkkrpacific:06546223540@cluster0.ifa043j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 .then(()=>console.log("connected to db"))
 .catch((err)=>console.error("connection failed",err));
 
