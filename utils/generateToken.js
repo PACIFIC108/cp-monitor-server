28500b16d585cbdde7180b0cb910da1aaf8d7a5e
PACIFIC108 <138829228+PACIFIC108@users.noreply.github.com>
2025-08-20 10:59:29 +0530

    Update generateToken.js

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -5,8 +5,8 @@ const generateToken = (res, userId) => {
 
     res.cookie('token', token, {
         httpOnly: true,
-        sameSite: "none",
-        secure: true,
+        sameSite: "strict",
+        // secure: true,
         maxAge: 7*24*60*60*1000
     });
 };
