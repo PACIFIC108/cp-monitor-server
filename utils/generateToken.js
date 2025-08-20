366097af5a95a824a66d77a253ec94f1ec8588ca
PACIFIC108 <138829228+PACIFIC108@users.noreply.github.com>
2025-08-20 09:05:16 +0530

    Update generateToken.js

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -4,9 +4,9 @@ const generateToken = (res, userId) => {
     const token = jwt.sign({ Id: userId }, process.env.JWT_SECRET, { expiresIn: '10h' });
 
     res.cookie('token', token, {
-        httpOnly: true,
-        sameSite: "none",
-        secure: true,
+        // httpOnly: true,
+        // sameSite: "none",
+        // secure: true,
         maxAge: 10*60*60*1000
     });
 };
